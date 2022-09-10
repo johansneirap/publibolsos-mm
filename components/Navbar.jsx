ce5d4288e39efe7ae6de9b016846a58de0a61396
Johans Neira <johansneirap@gmail.com>
2022-09-10 20:28:18 -0300

    fix: update logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,30 +27,38 @@ export const Navbar = () => {
   return (
     <nav className={styles["navbar"]}>
       <Link href="/">
-        <motion.a 
-          href="#" 
-          className={ styles.logo }
+        <motion.a
+          href="#"
+          className={styles.logo}
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: "easeInOut", delay: 0}} >
-          <img src={ logo.src } alt="logo" width="200" height=""/>
+          transition={{ duration: 1, ease: "easeInOut", delay: 0 }}
+        >
+          <img src={logo.src} alt="logo" width="200" height="" />
         </motion.a>
       </Link>
-      <input type="checkbox" id="toggler" className={ styles.toggler } />
-      <label htmlFor="toggler"><i className="fa fa-bars"></i></label>
-      <motion.div 
-        className="menu">
-        <motion.ul 
-          className={ styles.list }
+      <input type="checkbox" id="toggler" className={styles.toggler} />
+      <label htmlFor="toggler">
+        <i className="fa fa-bars"></i>
+      </label>
+      <motion.div className="menu">
+        <motion.ul
+          className={styles.list}
           initial={{ opacity: 0 }}
-          animate={{ opacity:1 }}
-          transition={{ duration: 1.5, ease: "easeInOut"}} >
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+        >
           {menuItems.map(({ text, href }, idx) => (
-            <motion.li 
+            <motion.li
               key={text}
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.2, ease: "easeInOut", delay: idx * 0.2 }}>
+              transition={{
+                duration: 1.2,
+                ease: "easeInOut",
+                delay: idx * 0.2,
+              }}
+            >
               <ActiveLink href={href} text={text} />
             </motion.li>
           ))}
