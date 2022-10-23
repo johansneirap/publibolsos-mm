210217924118610daa0c4b4199eeacdea05db0ea
Johans Neira <johansneirap@gmail.com>
2022-10-23 11:52:57 -0300

    fix: alert for sending mail

--- components/contact/Form.jsx
+++ components/contact/Form.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react"
+import { useState } from "react";
 import { InputText } from "./InputText";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
 
 const formInitialValues = {
-    name: "",
-    email: "",
-    message: "",
-    subject: "",
-}
-
+  name: "",
+  email: "",
+  message: "",
+  subject: "",
+};
 
 const fadeInBottomToTop = {
-    initial: { y: 50, opacity: 0 },
-    animate: { y: 0, opacity: 1 },
-    transition: { duration: 1.5, ease: "easeInOut", delay: 0.0 }
-}
+  initial: { y: 50, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  transition: { duration: 1.5, ease: "easeInOut", delay: 0.0 },
+};
 
 // const placeholderByField = {
 //     name: "Tu nombre",
@@ -24,95 +26,132 @@ const fadeInBottomToTop = {
 // }
 
 const formStructure = {
-    name: {
-        type: "text",
-        name: "name",
-        placeholder: "Tu nombre"
-    },
-    email: {
-        type: "email",
-        name: "email",
-        placeholder: "Email"
-    },
-    subject: {
-        type: "text",
-        name: "subject",
-        placeholder: "Asunto"
-    },
-    message: {
-        type: "textarea",
-        name: "message",
-        placeholder: "Deja tu mensaje aquí..."
-    }
-}
+  name: {
+    type: "text",
+    name: "name",
+    placeholder: "Tu nombre",
+  },
+  email: {
+    type: "email",
+    name: "email",
+    placeholder: "Email",
+  },
+  subject: {
+    type: "text",
+    name: "subject",
+    placeholder: "Asunto",
+  },
+  message: {
+    type: "textarea",
+    name: "message",
+    placeholder: "Deja tu mensaje aquí...",
+  },
+};
 
 export const Form = () => {
-    const [formState, setFormState] = useState(formInitialValues);
+  const [formState, setFormState] = useState(formInitialValues);
 
-    const handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
 
-        setFormState({
-            ...formState,
-            [target.name]: target.value
-        })
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formState);
+    let raw = JSON.stringify(formState);
 
-        // let data = formState;
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
 
-        let raw = JSON.stringify(formState);
+    MySwal.fire({
+      title: <p>Enviando correo...</p>,
 
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
+      didOpen: () => {
+        MySwal.showLoading();
+        fetch(
+          "https://oy7lzik2fxa56qyewvo6htaeki0mdwxu.lambda-url.us-east-1.on.aws/",
+          requestOptions
+        )
+          .then((response) => response.text())
+          .then(() => setFormState(formInitialValues))
+          .then(() =>
+            MySwal.fire(
+              "Correo enviado!",
+              "Nos pondremos brevemente en contacto contigo!",
+              "success"
+            )
+          )
+          .catch(() =>
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "No pudimos enviar tu correo",
+              footer:
+                "Estamos investigando la situacion, mientras tanto puedes intentar comunicarte por cualquiera de nuestros canales de comunicación",
+            })
+          );
+      },
+    })
+      .then(() => {
+        return MySwal.fire(
+          "Correo enviado!",
+          "Nos pondremos brevemente en contacto contigo!",
+          "success"
+        );
+      })
+      .catch(() => {
+        return Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "No pudimos enviar tu correo",
+          footer:
+            "Estamos investigando la situacion, mientras tanto puedes intentar comunicarte por cualquiera de nuestros canales de comunicación",
+        });
+      });
+  };
 
-        fetch("https://oy7lzik2fxa56qyewvo6htaeki0mdwxu.lambda-url.us-east-1.on.aws/", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .then(() => setFormState(formInitialValues))
-        .then(() => alert("Mensaje enviado"))
-        .catch(error => console.log('error', error));
+  return (
+    <motion.form action="" {...fadeInBottomToTop}>
+      <h2 className="form-title">Estamos siempre abiertos a para ti 24/7</h2>
+      {Object.keys(formInitialValues).map(
+        (field) =>
+          field !== "message" && (
+            <InputText
+              key={field}
+              placeholder={formStructure[field].placeholder}
+              name={field}
+              value={formState[field]}
+              handleChange={handleChange}
+            />
+          )
+      )}
+      <textarea
+        placeholder="Deja tu mensaje aquí..."
+        className="input-primary"
+        name="message"
+        id="message"
+        cols="30"
+        rows="10"
+        value={formState.message}
+        onChange={handleChange}
+      ></textarea>
 
-    }
-
-    return (
-        <motion.form 
-            action=""
-            { ...fadeInBottomToTop }>
-            <h2 className="form-title">Estamos siempre abiertos a para ti 24/7</h2>
-            {
-                Object.keys(formInitialValues).map(field => (
-                    field !== 'message' &&
-                    <InputText key={ field } placeholder={ formStructure[field].placeholder }
-                        name={ field }
-                        value={ formState[field] }
-                        handleChange={ handleChange }/>
-                ))
-            }
-            <textarea 
-                placeholder="Deja tu mensaje aquí..." 
-                className="input-primary" 
-                name="message" 
-                id="message" 
-                cols="30" rows="10"
-                value={ formState.message }
-                onChange={ handleChange }>
-            </textarea>
-
-            <button 
-                type="submit" 
-                className="main-button w-100"
-                onClick={ handleSubmit }>
-                    Enviar Mensaje
-            </button>
-        </motion.form>
-    )
-}
+      <button
+        type="submit"
+        className="main-button w-100"
+        onClick={handleSubmit}
+      >
+        Enviar Mensaje
+      </button>
+    </motion.form>
+  );
+};
